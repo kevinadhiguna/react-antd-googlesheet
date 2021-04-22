79e60861e5ae1b36449d3c0ab88d266eacae812c
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-23 00:37:07 +0700

    style: add tail form item layout

--- src/App.tsx
+++ src/App.tsx
@@ -58,6 +58,19 @@ const formItemLayout = {
   },
 };
 
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
+
 function App() {
   return (
     <>
