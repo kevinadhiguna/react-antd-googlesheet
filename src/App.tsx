5dff8d9d822e18a8a1c57b82373ce54cb40ca433
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-23 00:42:22 +0700

    refactor: add prefixSelector for phone number

--- src/App.tsx
+++ src/App.tsx
@@ -78,6 +78,15 @@ function App() {
     console.info("Received values of form :", values);
   }
 
+  const prefixSelector = (
+    <Form.Item name="prefix" noStyle>
+      <Select style={{ width: 70 }}>
+        <Option value="1">+1</Option>
+        <Option value="81">+81</Option>
+      </Select>
+    </Form.Item>
+  );
+
   return (
     <>
       <Typography>
